67d49a0c5cd2ca7d3850754143f57b560707e10d
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-06-12 14:38:36 +0200

    SIPA-12 Update Category Controller + Model

---
+++ src/main/java/com/example/onlineshop/model/Category.java
@@ -0,0 +1,9 @@
+package com.example.onlineshop.model;
+
+public record Category(Long id, Long parentId, String name) {
+
+    public String category() {
+        return "id  " + id + "Parentid" + parentId + "Name" + name;
+    }
+
+}
