9ce2798b4b706621dedba8298a3b56d856f5b0f5
kamil.szestowicki <kamil.szestowicki@gmail.com>
2022-06-26 10:34:47 +0200

    Simple test

--- src/main/java/com/example/onlineshop/controller/BasketItemController.java
+++ src/main/java/com/example/onlineshop/controller/BasketItemController.java
@@ -1,55 +1,38 @@
 package com.example.onlineshop.controller;
 
-import com.example.onlineshop.model.BasketItem;
 import com.example.onlineshop.dto.BasketItemDTO;
-import org.springframework.web.bind.annotation.*;
+import com.example.onlineshop.model.BasketItem;
+import com.example.onlineshop.service.BasketItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/basketItem")
 public class BasketItemController {
+    private BasketItemService service;
 
-    public BasketItemController(){}
-
-    private AtomicLong index = new AtomicLong(0);
-
-    private List<BasketItem> basketItems = new LinkedList<>()
-    {{
-            add(new BasketItem(nextIdx(), 6L, 4, 20, 11));
-            add(new BasketItem(nextIdx(), 8L, 3, 10, 11));
-            add(new BasketItem(nextIdx(), 4L, 2, 30, 11));
-
-        }};
-
-    private long nextIdx() {
-        return index.incrementAndGet();
-    }
-
-    private double calculateValue(long quantity, double price) {
-        return quantity * price;
+    public BasketItemController(BasketItemService service) {
+        this.service = service;
     }
 
     @GetMapping
     public List<BasketItem> getBasketItems() {
-        return basketItems;
+        return service.getBasketItems();
     }
 
     @GetMapping("/{id}")
     public BasketItem getBasketItems(@PathVariable Long id) {
-        return basketItems.get(id.intValue() - 1);
+        return service.getItemBy(id);
     }
 
     @PostMapping()
     public Long addBasketItem(@RequestBody BasketItemDTO dto) {
-        System.out.println(dto);
-        BasketItem newBasketItem = dto.toBasketItem(nextIdx());
-        basketItems.add(newBasketItem);
-
-        return newBasketItem.id();
+        return service.addItem(dto);
     }
-
-
 }
--- src/test/java/com/example/onlineshop/OnlineShopApplicationTests.java
+++ src/test/java/com/example/onlineshop/OnlineShopApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.onlineshop;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class OnlineShopApplicationTests {
+@AutoConfigureMockMvc
+public class OnlineShopApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    protected MockMvc mockMvc;
 
 }
