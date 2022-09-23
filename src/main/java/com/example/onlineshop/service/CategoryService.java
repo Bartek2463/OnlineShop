7d5e7494c8cc27eacc20265e92cefcbadbc8c926
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-23 23:46:25 +0200

    departments of services and connecting to the database add to Tetst feature

--- src/main/java/com/example/onlineshop/service/CategoryService.java
+++ src/main/java/com/example/onlineshop/service/CategoryService.java
@@ -1,17 +1,30 @@
 package com.example.onlineshop.service;
 
-import com.example.onlineshop.dto.CategoryDTO;
 import com.example.onlineshop.model.Category;
+import com.example.onlineshop.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
+@AllArgsConstructor
 public class CategoryService {
 
+private CategoryRepository categories;
 
+    public List<Category> getCategories() {
+        return categories.findAll();
+    }
+
+    public Long getCategoryById(Long id) {
+        return categories.findById(id).orElse(null).getId();
+    }
+
+    public Category addCategory(Category category) {;
+
+        return categories.save(category);
+    }
 
 
 }
