15a3c7d02a9a87d07db920a5c29cd1ea0c5bbfbf
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-09-29 12:35:58 +0200

    Create test  Mock and InjectMock using MockitoExtension class

--- src/test/java/com/example/onlineshop/service/CategoryServiceTest.java
+++ src/test/java/com/example/onlineshop/service/CategoryServiceTest.java
@@ -1,34 +1,32 @@
 package com.example.onlineshop.service;
 
-import com.example.onlineshop.OnlineShopApplicationTests;
+import com.example.onlineshop.dto.CategoryDTO;
 import com.example.onlineshop.model.Category;
-import com.example.onlineshop.model.Product;
-import com.example.onlineshop.service.CategoryService;
-import org.assertj.core.api.Assert;
+import com.example.onlineshop.repository.CategoryRepository;
 import org.junit.jupiter.api.BeforeAll;
-
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-
-import static org.apache.logging.log4j.ThreadContext.isEmpty;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNullPointerException;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
+@ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
 
+    private static final Category category = new Category(1l, null, "Inne");
+    private static final CategoryDTO dto = new CategoryDTO(1l, 1l, "AGD");
 
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryService categoryService;
 
 
-    }
+}
 
 
 
